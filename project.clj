f03d1ea6317b5327cfef23d184dd8abdf9016db0
mattyw <mattyw@me.com>
2013-10-22 20:25:29 -0700

    starting to cleanup

--- project.clj
+++ project.clj
@@ -5,5 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :profiles {:dev {:dependencies [[speclj "2.2.0"]]}}
-  :plugins [[speclj "2.2.0"]]
-  :test-paths ["spec"])
+  :plugins [[speclj "2.2.0"]])
--- src/gilded_rose/core.clj
+++ src/gilded_rose/core.clj
@@ -1,10 +1,19 @@
 (ns gilded-rose.core)
 
+(defn reduce-sell-in [items]
+  (map (fn [item]
+        (if (not= "Sulfuras, Hand of Ragnaros" (:name item))
+          (merge item {:sell-in (dec (:sell-in item))})
+          item))
+    items))
+
 (defn update-quality [items]
   (map
-    (fn[item] (cond
+    (fn[item] 
+      (cond
       (and (< (:sell-in item) 0) (= "Backstage passes to a TAFKAL80ETC concert" (:name item)))
         (merge item {:quality 0})
+
       (or (= (:name item) "Aged Brie") (= (:name item) "Backstage passes to a TAFKAL80ETC concert"))
         (if (and (= (:name item) "Backstage passes to a TAFKAL80ETC concert") (>= (:sell-in item) 5) (< (:sell-in item) 10))
           (merge item {:quality (inc (inc (:quality item)))})
@@ -13,20 +22,20 @@
             (if (< (:quality item) 50)
               (merge item {:quality (inc (:quality item))})
               item)))
+
       (< (:sell-in item) 0)
         (if (= "Backstage passes to a TAFKAL80ETC concert" (:name item))
           (merge item {:quality 0})
           (if (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
             (merge item {:quality (- (:quality item) 2)})
             item))
+
       (or (= "+5 Dexterity Vest" (:name item)) (= "Elixir of the Mongoose" (:name item)))
         (merge item {:quality (dec (:quality item))})
+
       :else item))
-  (map (fn [item]
-      (if (not= "Sulfuras, Hand of Ragnaros" (:name item))
-        (merge item {:sell-in (dec (:sell-in item))})
-        item))
-  items)))
+
+  (reduce-sell-in items)))
 
 (defn item [item-name, sell-in, quality]
   {:name item-name, :sell-in sell-in, :quality quality})
